f854fd2d84d5a9e4c5446254650a30723a175db0
Arthdevhal <148552438+ArthdevhalNekiu@users.noreply.github.com>
2024-01-10 15:27:08 -0300

    Update Arthdevhal.py

--- Arthdevhal.py
+++ Arthdevhal.py
@@ -16,3 +16,23 @@ class very_original_pet:
 
     def pet_does(self, sound = "woof"):
         print(self.pet_is, self.pet_name, sound)
+
+
+def get_from_repository(repository: str, get: str) -> "":
+    start = repository.find(get)
+    end = repository[start+1:].find("\n"*3)+1+start
+    return (repository[start:end])
+
+
+def hello():
+    print("Hello World")
+
+
+def how():
+    print("How are you?")
+
+
+def some_def():
+    print("-"*30)
+    print("Some random function")
+    print("-"*30)
